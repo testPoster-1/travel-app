62f06cc4ef438e1417126b0beadaa7ecc8cade38
= <anguyen031@gmail.com>
2021-05-14 12:10:15 -0700

    Add html webpack plugin
    Plugin generates HTML5 file that includes all webpack bundles using
    appropriate tags. Allow use of html tags in build.

--- src/server/server.js
+++ src/server/server.js
@@ -11,6 +11,9 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 app.use(express.static("dist"));  //entry point for app
+app.get("/", function (req, res) {
+  res.sendFile(path.resolve("dist/index.html"))
+  }); //resolves the path to the html file to render into browser
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);;
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,9 +1,16 @@
 const path = require("path"); 
 const webpack = require("webpack");
+const htmlWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
   mode: "development",
   devServer: { 
     port: 3000 
-  }
+  }, 
+  plugins: [ 
+    new htmlWebpackPlugin({ 
+      template: "./src/client/views/index.html", 
+      filename: "./index.html" 
+    }) 
+  ] 
 } 
