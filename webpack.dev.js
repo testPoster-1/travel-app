56c03dcf21ccee1df1978ff0910ac83c13c12d89
= <anguyen031@gmail.com>
2021-05-14 11:38:43 -0700

    Add clean-webpack plugin
    Automatically removes the old build folder on each rebuild to save time

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,4 +3,7 @@ const webpack = require("webpack");
 
 module.exports = {
   mode: "development",
+  devServer: { 
+    port: 3000 
+  }
 } 
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,9 +1,16 @@
 const path = require("path");
 const webpack = require("webpack"); 
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   mode: "production",
   entry: "./src/client/index.js",
+  output: { 
+    path: path.join(__dirname, 'dist'), 
+    filename: 'main.js', 
+    libraryTarget: 'var', 
+    library: 'Client'
+  },
   module: { 
     rules: [ 
       { test: '/.js$/', 
@@ -11,5 +18,13 @@ module.exports = {
         loader: "babel-loader" 
       } 
     ] 
-  }
+  }, 
+  plugins: [
+    new CleanWebpackPlugin ({
+      dry: true, 
+      verbose: true,
+      cleanStaleWebpackAssets: true,
+      protectWebpackAssets: false
+    })
+  ]
 } 
