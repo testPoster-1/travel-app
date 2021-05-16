40f2444a735d8c874496b78b42fb77553f0288ef
= <anguyen031@gmail.com>
2021-05-16 07:11:44 -0700

    Correct calculations in timeDifference function
    Difference between user input date and today's date was not calculated
    correctly in the initial function version.

--- src/client/js/getTime.js
+++ src/client/js/getTime.js
@@ -1,16 +1,21 @@
 export const timeDifference = () => {
   let d = new Date(); //gets current date and time to compare against the user's date
-  let leaveDate = document.getElementById("leave-date").value;
+  let inputDate = new Date(document.getElementById("leave-date").value);
   let dateToday = (`${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate()}`); //Note +1 in order to get the actual month 
-console.log(dateToday);
-const timeDiff = new Date(leaveDate).getTime() - new Date(dateToday).getTime() //difference in milliseconds
-console.log(timeDiff);
+  let leaveDate = (`${inputDate.getFullYear()}-${inputDate.getMonth() + 1}-${inputDate.getDate()+1}`);
+  console.log(`${dateToday} + ${leaveDate}`);
+  const timeDiff = new Date(leaveDate).getTime() - new Date(dateToday).getTime() //difference in milliseconds
+  console.log(timeDiff);
 
-if (timeDiff > 518400000) { //milliseconds - cut off is 6 days
-  console.log("longer than 6 days aways");
-  return true
+  if (timeDiff > 518400000) { //milliseconds - cut off is 6 days
+    console.log("longer than 6 days aways");
+    return 1;
+  } else if (timeDiff < 0) {
+    console.log("Input date is in the past");
+    return -1; 
   } else {
-    console.log("6 days or shorter away");
-    return false
+    console.log("Date is 6 days or less away");
+    return 0;
   }
 }
+
