c8dbd3600096c37714c6f81ea1325030f80179ac
= <anguyen031@gmail.com>
2021-05-19 16:35:00 -0700

    Add image via queryselector

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,4 +1,4 @@
-import { weatherbitFetch } from "./weatherbitFetch";
+
 import { updateUI } from "./updateUI";
 import { timeDifference } from "./InputTesting/dateValidation";
 import { PixabayImages } from "./getImages";
@@ -6,20 +6,6 @@ import { nameValidation } from "./InputTesting/nameValidation";
 import { destValidation } from "./InputTesting/destValidation";
 
 export const handleSubmit = (e) => {
-  let preLoader = document.getElementById("pre-loader-holder"); //play the loading animation
-  preLoader.classList.add("pre-loader");
-  let loadCounter = 4;
-
-  let loadTimer = setInterval(function(){ //create a setinterval function as a countdown. When countdown reaches 0, the loading animation will be removed. This will coincide with the user info appearing in the UI. 
-    if(loadCounter <= 0){
-      clearInterval(loadTimer);
-      preLoader.classList.remove("pre-loader");
-    } else {
-      loadCounter -= 1;
-    }
-  }, 500);
-  
-
   e.preventDefault(); //prevent page from reloading on click  
   document.getElementById("err-holder").innerHTML = "";
   let userDate = document.getElementById("leave-date").value;
@@ -27,8 +13,11 @@ export const handleSubmit = (e) => {
   let userCity = document.getElementById("city").value;
   let userState = document.getElementById("state").value;
   let userCountry = document.getElementById("country").value;
-  let userDest = (`${userCity},${userState},${userCountry}`);
+  let userDest = (`${userCity},${userState},${userCountry}`); 
   
+  const imgHolder = document.querySelector("#img-holder");
+  imgHolder.style.backgroundImage = ``;
+
   console.log("User clicked submit");
   console.log(`location: ${userDest}`);
   console.log(`Leave Date: ${userDate}`);
@@ -37,12 +26,27 @@ export const handleSubmit = (e) => {
   let interval = timeDifference(userDate);
   let name = nameValidation(userName);
   let destination = destValidation(userCity);
-   
+
   if (name && interval && destination) {
+    let preLoader = document.getElementById("pre-loader-holder"); //play the loading animation
+  preLoader.classList.add("pre-loader");
+  let loadCounter = 2;
+  document.getElementById("text").innerHTML = "NOW LOADING";
+
+  let loadTimer = setInterval(function(){ //create a setinterval function as a countdown. When countdown reaches 0, the loading animation will be removed. This will coincide with the user info appearing in the UI. 
+    if(loadCounter <= 0){
+      clearInterval(loadTimer);
+      preLoader.classList.remove("pre-loader");
+      document.getElementById("text").innerHTML = "";
+    } else {
+      loadCounter -= 1;
+    }
+  }, 600);
+  
     console.log("all is true")
     postData(userDest, interval)
       //.then (coords => weatherbitFetch(coords));
-      .then(dataObj => updateUI(dataObj));
+      .then(dataObj => updateUI(dataObj, imgHolder));
   }
 }
 
--- src/client/js/updateUI.js
+++ src/client/js/updateUI.js
@@ -1,12 +1,3 @@
-export const updateUI = (dataObj) => {
- 
-
-  console.log(JSON.stringify(dataObj));
-  console.log("UpdateUI function ran");
-
- 
-  const imgHolder = document.getElementById("img-holder");
-  imgHolder.src = String("");
-  imgHolder.src = String(dataObj.pixURL);
-  console.log(`this is my pix url ${dataObj.pixURL}`);
+export const updateUI = (dataObj, imgHolder) => {
+  imgHolder.style.backgroundImage = `url(${dataObj.pixURL})`;
 }
