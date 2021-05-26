fce4a68baf05f2d8be01bb9e233f70cb0c4cd1fe
= <anguyen031@gmail.com>
2021-05-26 06:58:28 -0700

    Stopping pt: check local storage for item

--- src/client/js/deleteTrip.js
+++ src/client/js/deleteTrip.js
@@ -1,5 +1,6 @@
 export const deleteTrip = () => {
   if (confirm("Are you sure you want to remove all of your trip information?")) {
+    localStorage.clear();   // clear local storage
     window.location.reload();
   };
   
--- src/client/js/localStorage.js
+++ src/client/js/localStorage.js
@@ -1,15 +1,23 @@
 export const persistance = (tripData) => {
   console.log("persistance ran");
-  let tripArray = localStorage.getItem('trips') ? JSON.parse(localStorage.getItem('trips')) : [];  //ternary operator: if localstorage.getitem(items) is true, then triparray will equal JSON.parse(localStorage.getItem('items'), if it's false, then triparry will be equal to []. 
 
-const data = JSON.parse(localStorage.getItem('trips'));
-// tripArray.push("hello");
-tripArray.push(tripData);
-localStorage.setItem('trips', JSON.stringify(tripArray));
-console.log(data);
+  //let tripArray = [];
+
+  let tripArray = localStorage.getItem('trips') ? JSON.parse(localStorage.getItem('trips')) : []; //ternary operator: if localstorage.getitem(items) is true, then triparray will equal JSON.parse(localStorage.getItem('items'), if it's false, then triparry will be equal to []. 
 
 
-//localStorage.clear();   clear local storage
+  if (localStorage.getItem('trips')== (JSON.stringify(tripArray))) {
+    console.log("Already there"); 
+    console.log(tripArray); 
+  } else {
+    const data = JSON.parse(localStorage.getItem('trips'));
+    // tripArray.push("hello");
+ tripArray.push(tripData);
+    console.log(tripArray);
+    localStorage.setItem('trips', JSON.stringify(tripArray));
+    console.log(data);
+  }
 
-
+  localStorage.clear(); 
+  console.log("cleared: " + JSON.stringify(tripArray));
 }
