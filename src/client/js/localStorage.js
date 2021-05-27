dc380fb643bb1b2275b4e6f777cb9c3c764f0ad3
= <anguyen031@gmail.com>
2021-05-26 18:10:28 -0700

    Push obj into array; delete all obj

--- src/client/js/localStorage.js
+++ src/client/js/localStorage.js
@@ -1,26 +1,29 @@
 export const persistance = (tripData) => {
   console.log("persistance ran");
 
-//tripArray = [];
+  //tripArray = [];
 
   let tripArray = localStorage.getItem('trips') ? JSON.parse(localStorage.getItem('trips')) : []; //ternary operator: if localstorage.getitem(items) is true, then triparray will equal JSON.parse(localStorage.getItem('items'), if it's false, then triparry will be equal to []. 
 
   let add = addData();
-function addData () { 
-for (let trip in tripArray) {
-  console.log(tripArray[trip].fetchedData.city);
-  console.log(tripData.fetchedData.city);
-  let arraycity = tripArray[trip].fetchedData.city;
-  if (arraycity == tripData.fetchedData.city) {
-    console.log("I'm there");
-    return true;
-  }   
-}
+  function addData() {
+    for (let trip in tripArray) {
+      console.log(tripArray[trip].fetchedData.city);
+      console.log(tripData.fetchedData.city);
+      let arraycity = tripArray[trip].fetchedData.city;
+      if (arraycity == tripData.fetchedData.city) {
+        console.log("I'm there");
+        return true;
+      }
+    }
 
-}
-if (add == true) {
-  console.log("it's there");
-} else {
-  console.log("I can be added");
-}
+  }
+  if (add == true) {
+    console.log("it's there");
+  } else {
+    tripArray.push(tripData);
+    localStorage.setItem('trips', JSON.stringify(tripArray));
+    const data = JSON.parse(localStorage.getItem('trips'));
+    console.log(data);
+  }
 }
