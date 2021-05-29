2c029abbcf1f2a6183ad1bc618397a7647cb5985
= <anguyen031@gmail.com>
2021-05-28 19:17:20 -0700

    Fixed for loop for saved cities buttons

--- src/client/index.js
+++ src/client/index.js
@@ -1,14 +1,15 @@
 import {handleSubmit} from "./js/main";
 require("./styles/styles.css");
 if (localStorage.getItem('trips')) {
-  for (let i = 0; i < localStorage.length+1; i++) {
+  console.log(JSON.parse(localStorage.getItem("trips")).length);
+  console.log(localStorage.length);
+  for (let i = 0; i < JSON.parse(localStorage.getItem("trips")).length; i++) { //need to JSON.parse the local storage first, then find the length.  
     let savedData = JSON.parse(localStorage.getItem("trips")); //json.parse retrieves the item from storage and converts it to object again 
-    console.log(i);
     console.log(savedData[i].fetchedData.city);
     let savedBtn = document.createElement("button");
     savedBtn.classList.add("btn-style", "trip-info");
     savedBtn.innerHTML = savedData[i].fetchedData.city;
-    document.getElementById("saved-div").appendChild(savedBtn);
+    document.getElementById("saved-cities").appendChild(savedBtn);
   }
 } else {
   document.getElementById("saved").innerHTML = "You have no saved trips";
