2a574b2be30adbe05ea0dfafe7fb53778b22292b
= <anguyen031@gmail.com>
2021-06-06 18:47:11 -0700

    Create README file

---
+++ README.md
@@ -0,0 +1,35 @@
+# Travel App
+This project utilizes **JavaScript**, **HTML**, **Sass** to provide weather data for upcoming trips. 
+
+Enter your name, destination city/state/country, departure and return dates. The app will calculate your trip length and provide a weather forecast. The number of days in the weather forecast depends upon when you are leaving for your trip. Trip information can also be saved, retrieved, updated, and deleted.
+
+## Version 1.0
+
+## APIs used in this project 
+- GeoNames 
+- Weatherbit
+- Pixabay
+
+## Instructions to run app 
+- Obtain API keys from the above sites 
+- Create a `.env` file in the root directory 
+- Copy the following lines of code into the `.env` file and input your API keys where prompted
+
+  geo_username = <API key here>
+  weather_API_KEY = <API key here>
+  pixabay_API_KEY = <API key here>  
+
+## To start production mode  
+  npm run prod
+
+## To start the server 
+  npm run start
+
+## To start development mode  
+  npm run dev
+
+## Jest testing 
+  npm jest
+
+## Copyright
+_FreudCat 2021_
--- src/client/js/savedTrips.js
+++ src/client/js/savedTrips.js
@@ -2,24 +2,27 @@
 import { updateUI } from "./updateUI";
 
 export const savedTrips = () => { //creates the buttons for users to get data from saved trips or lets them know there are no saved trips
-let saveText = document.getElementById("save-text"); 
-if ((localStorage.getItem('trips'))) { 
-  for (let i = 0; i < JSON.parse(localStorage.getItem("trips")).length; i++) { //NOTE: need to do JSON.parse to get the length into a num versus a string 
-    let savedData = JSON.parse(localStorage.getItem("trips")); 
-    console.log("length: " + savedData.length);
-    console.log(savedData);
-    let savedBtn = document.createElement("button");
-    savedBtn.classList.add("btn-style", "trip-info");
-    savedBtn.id = (`city${i}`);
-    savedBtn.innerHTML = savedData[i].dataObj.fetchedData.city;
-    document.getElementById("saved-cities").appendChild(savedBtn);
 
-    savedBtn.addEventListener("click", function (e) {
-      updateUI(savedData[i]);
-      document.getElementById("save-trip").classList.add("hidden");
-    })
-  }
-} else {
-  saveText.innerHTML = "You have no saved trips";
-};
+  let saveText = document.getElementById("save-text");
+
+  if ((!localStorage.getItem('trips')) || JSON.parse(localStorage.getItem('trips')).length == 0) { //the first statement will work if the user has opened the app for the first time and the localstorage will not have been created, yet, and will therefore be undefined. The second statement will work if the user has already utilized local storage (so it is created and no longer undefined), but they have deleted everything so the localstorage is empty with length of 0. Order of statements is important here.  
+    saveText.innerHTML = "You have no saved trips";
+    
+  } else {
+
+    for (let i = 0; i < JSON.parse(localStorage.getItem("trips")).length; i++) { //NOTE: need to do JSON.parse to get the length into a num versus a string 
+      let savedData = JSON.parse(localStorage.getItem("trips"));
+      let savedBtn = document.createElement("button");
+      savedBtn.classList.add("btn-style", "trip-info");
+      savedBtn.id = (`city${i}`);
+      savedBtn.innerHTML = savedData[i].dataObj.fetchedData.city;
+      document.getElementById("saved-cities").appendChild(savedBtn);
+
+      savedBtn.addEventListener("click", function (e) {
+        updateUI(savedData[i]);
+        document.getElementById("save-trip").classList.add("hidden");
+      })
+    }
+
+  };
 }
